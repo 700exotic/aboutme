e483af995a28aea2d06f40e9345b59e589207731
myfundareliquidated <86858354+myfundareliquidated@users.noreply.github.com>
2025-07-17 21:41:40 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const listItems = document.querySelectorAll('.info-list li');
+
+listItems.forEach(item => {
+  const originalText = item.textContent;
+
+  item.addEventListener('mouseenter', () => {
+    item.textContent = item.dataset.hover;
+  });
+
+  item.addEventListener('mouseleave', () => {
+    item.textContent = originalText;
+  });
+});
+
+// Lower the volume of background music
+const music = document.getElementById('bg-music');
+music.volume = 0.1; // 10% volume
